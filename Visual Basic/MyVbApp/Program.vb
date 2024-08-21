3830a6d0368274a18d768631978712a181ac7052
hongch666 <129023404+hongch666@users.noreply.github.com>
2024-08-21 23:16:48 +0800

    Add files via upload

---
+++ Visual Basic/MyVbApp/Program.vb
@@ -0,0 +1,8 @@
+Imports System
+
+Module Program
+    Sub Main(args As String())
+    ' 输入指令dotnet new console -lang VB -o MyVbApp（这个可以换其他名字）创建VB项目
+        Console.WriteLine("Hello World!")
+    End Sub
+End Module
